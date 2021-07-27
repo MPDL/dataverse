27aa5c1f9ec1651f0772f30d89b8e5e928a45b71
vilarodr <vilarodriguez@mpdl.mpg.de>
2021-07-27 09:32:52 +0200

    #2 License options for Data licence Germany added

--- src/main/java/edu/harvard/iq/dataverse/TermsOfUseAndAccess.java
+++ src/main/java/edu/harvard/iq/dataverse/TermsOfUseAndAccess.java
@@ -272,7 +272,7 @@ public class TermsOfUseAndAccess implements Serializable {
     
     // TODO Vila
     public enum License {
-        NONE, CC0, CCBY, CCBYSA, PDDL, ODCBY, ODBL
+        NONE, CC0, CCBY, CCBYSA, PDDL, ODCBY, ODBL, DLGA, DLGZ
     }
     
         /**
