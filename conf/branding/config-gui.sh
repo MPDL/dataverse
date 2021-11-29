eb57319108e4297e3563d0165b58e9b15704cb98
vilarodr <vilarodriguez@mpdl.mpg.de>
2021-11-29 15:59:14 +0100

    Titillium Fonts as local resource

--- conf/branding/config-gui.sh
+++ conf/branding/config-gui.sh
@@ -28,93 +28,121 @@ if [[ $# -gt 0 ]]; then
       if [[ $# -eq 2 ]]; then
         unblock="?unblock-key=$2"
       else
-        echo $usage
+        printf "\n$usage\n"
         exit 1
       fi
       ;;
     *)
-      echo $usage
+      printf "\n$usage\n"
       exit 1
-      ;;      
+      ;;
   esac
 fi
 
-echo "Branding Installation ..."
+log="/tmp/`basename $0`.`date +"%s"`.log";
+
+printf "\n\nBranding Installation (process output to -> $log)\n" | tee -a $log
+
+printf "\n\nPreparing destination folders:\n" | tee -a $log
 
 if [ ! -d "$destinationDocroot/logos/navbar" ]; then
   mkdir -p $destinationDocroot/logos/navbar
   if [ $? -eq 0 ]; then
-    echo "$destinationDocroot/logos/navbar created"
+    printf "\n$destinationDocroot/logos/navbar created" | tee -a $log
     status="0"
   else
+    printf "\n$destinationDocroot/logos/navbar could not be created!" | tee -a $log
     status="1"
   fi
 else
+  printf "\n$destinationDocroot/logos/navbar already exists" | tee -a $log
   status="0"
 fi
 
 if [ ! -d "$destinationDocroot/logos/images" ]; then
   mkdir -p $destinationDocroot/logos/images
   if [ $? -eq 0 ]; then
-    echo "$destinationDocroot/logos/images created"
+    printf "\n$destinationDocroot/logos/images created" | tee -a $log
     status+=0
   else
+    printf "\n$destinationDocroot/logos/images could not be created!" | tee -a $log
     status+=1
   fi
 else
+  printf "\n$destinationDocroot/logos/images already exists" | tee -a $log
+  status+=0
+fi
+
+if [ ! -d "$destinationDocroot/logos/fonts" ]; then
+  mkdir -p $destinationDocroot/logos/fonts
+  if [ $? -eq 0 ]; then
+    printf "\n$destinationDocroot/logos/fonts created" | tee -a $log
+    status+=0
+  else
+    printf "\n$destinationDocroot/logos/fonts could not be created!" | tee -a $log
+    status+=1
+  fi
+else
+  printf "\n$destinationDocroot/logos/fonts already exists" | tee -a $log
   status+=0
 fi
 
 if [ ! -d "$destinationCustomizationFiles" ]; then
   mkdir -p $destinationCustomizationFiles
   if [ $? -eq 0 ]; then
-    echo "$destinationCustomizationFiles created"
+    printf "\n$destinationCustomizationFiles could not be created!" | tee -a $log
     status+=0
   else
+    printf "\n$destinationCustomizationFiles created" | tee -a $log
     status+=1
   fi
 else
+  printf "\n$destinationCustomizationFiles already exists" | tee -a $log
   status+=0
 fi
 
-echo "... copying resources to destination ..."
+printf "\n\nCopying resources to destination:\n" | tee -a $log
 
-cp -R resources/assets/* $destinationDocroot/logos/
+cp -R $projectHome/conf/branding/resources/assets/* $destinationDocroot/logos/
 if [ $? -eq 0 ]; then
-  echo "resources/assets/* copied to $destinationDocroot/logos/"
+  printf "\n$projectHome/conf/branding/resources/assets/* copied to $destinationDocroot/logos/" | tee -a $log
   status+=0
 else
+  printf "\nsome problem copying $projectHome/conf/branding/resources/assets/* to $destinationDocroot/logos/,this step failed!" | tee -a $log
   status+=1
 fi
 
-cp resources/css/*.css $destinationCustomizationFiles
+cp $projectHome/conf/branding/resources/css/*.css $destinationCustomizationFiles
 if [ $? -eq 0 ]; then
-  echo "resources/css/*.css copied to $destinationCustomizationFiles"
+  printf "\n$projectHome/conf/branding/resources/css/*.css copied to $destinationCustomizationFiles" | tee -a $log
   status+=0
 else
+  printf "\nsome problem copying $projectHome/conf/branding/resources/css/*.css to $destinationCustomizationFiles,this step failed!" | tee -a $log
   status+=1
 fi
 
-cp resources/*.html $destinationCustomizationFiles
+cp $projectHome/conf/branding/resources/*.html $destinationCustomizationFiles
 if [ $? -eq 0 ]; then
-  echo "resources/assets/*.html copied to $destinationCustomizationFiles"
+  printf "\n$projectHome/conf/branding/resources/assets/*.html copied to $destinationCustomizationFiles" | tee -a $log
   status+=0
 else
+  printf "\nsome problem copying $projectHome/conf/branding/resources/assets/*.html to $destinationCustomizationFiles,this step failed!" | tee -a $log
   status+=1
 fi
 
-echo "... setting paths ..."
-curl -X PUT -d "/logos/navbar/logo_for_bright.png" http://localhost:8080/api/admin/settings/:LogoCustomizationFile$unblock
-echo
-curl -X PUT -d "$destinationCustomizationFiles/mpdl-footer.html" http://localhost:8080/api/admin/settings/:FooterCustomizationFile$unblock
-echo
-curl -X PUT -d "$destinationCustomizationFiles/mpdl-stylesheet.css" http://localhost:8080/api/admin/settings/:StyleCustomizationFile$unblock
-echo
+printf "\n\nSetting paths:\n\n" | tee -a $log
+
+curl -X PUT -d "/logos/navbar/logo_for_bright.png" http://localhost:8080/api/admin/settings/:LogoCustomizationFile$unblock -q | tee -a $log
+printf "\n" | tee -a $log
+curl -X PUT -d "$destinationCustomizationFiles/mpdl-footer.html" http://localhost:8080/api/admin/settings/:FooterCustomizationFile$unblock -q | tee -a $log
+printf "\n" | tee -a $log
+curl -X PUT -d "$destinationCustomizationFiles/mpdl-stylesheet.css" http://localhost:8080/api/admin/settings/:StyleCustomizationFile$unblock -q | tee -a $log
+printf "\n" | tee -a $log
 
 if [ $status -eq 0 ]; then
   printf "\n... DONE!\n"
   exit 0
 else
-  printf "\n... sorry, some step fails!\nsome hints: $status...\n"
+  printf "\n... sorry, some step fails!\nsome hints: $status... for more info see $log\n"
   exit 2
 fi
