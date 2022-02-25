82d6bddcc7b29002ffbf88e38b3a41cb834ed7a9
vilarodr <vilarodriguez@mpdl.mpg.de>
2022-02-25 20:56:00 +0100

    mail on static pages secured

---
+++ conf/branding/resources/js/custom.js
@@ -0,0 +1,7 @@
+function getUrlFor (page) {
+    return window.location.href.replace(window.location.pathname, '').split('#')[0] + page;
+}
+
+function _2edmond() {
+    location.href = atob('bWFpbHRvOmVkbW9uZEBtcGRsLm1wZy5kZQ==');
+}
